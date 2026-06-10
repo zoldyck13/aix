1bed9aedcb16d928b596d7ce4c687c45c3177003
Ali <alifage14@gmail.com>
2026-06-10 05:19:49 +0300

    Update: add implemention for the http_client.cpp

---
+++ src/network/http_client.cpp
@@ -0,0 +1,35 @@
+#include "http_client.hpp"
+#include <curlpp/cURLpp.hpp>
+#include <curlpp/Easy.hpp>
+#include <curlpp/Options.hpp>
+#include <curlpp/Exception.hpp>
+#include <sstream>
+#include <list>
+
+
+namespace  aix {
+  std::string HttpClient::post(const std::string &url, const std::string &json_payload) {
+    curlpp::Cleanup cleanup;
+    curlpp::Easy request;
+
+    try {
+      request.setOpt(new curlpp::options::Url(url));
+      std::list<std::string> header;
+      header.push_back("Content-Type: application/json");
+      request.setOpt(new curlpp::options::HttpHeader(header));
+      request.setOpt(new curlpp::options::PostFields(json_payload));
+      request.setOpt(new curlpp::options::PostFieldSize(json_payload.length()));
+      std::ostringstream respones_stream;
+      request.setOpt(new curlpp::options::WriteStream(&respones_stream));
+      request.perform();
+      return respones_stream.str();
+    
+    } catch (curlpp::LogicError &e) {
+      return "Logic Error: " + std::string(e.what());
+    } catch (curlpp::RuntimeError &e) {
+      return "Runtime Error: " + std::string(e.what());
+    }
+
+  }
+
+}
--- src/network/http_client.hpp
+++ src/network/http_client.hpp
@@ -2,17 +2,13 @@
 #define HTTP_CLIENT_HPP
 
 #include <string>
-#include <curlpp/cURLpp.hpp>
-#include <curlpp/Easy.hpp>
-#include <curlpp/Options.hpp>
-#include <curlpp/Exception.hpp>
 
 
 namespace aix {
   class HttpClient {
     public:
-      HttpClient();
-      ~HttpClient();
+      HttpClient() = default;
+      ~HttpClient() = default;
 
       std::string post(const std::string& url, const std::string& json_payload);
   };
