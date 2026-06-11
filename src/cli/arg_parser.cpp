85b03d3c5fc0fc82efe9c783124167a54b134d65
Ali S. Mutar <136193544+zoldyck13@users.noreply.github.com>
2026-06-11 17:01:26 +0300

    Update: enable clang-format check on CI, update src/ tests/ (#15)

--- src/cli/arg_parser.cpp
+++ src/cli/arg_parser.cpp
@@ -1,32 +1,33 @@
 #include "arg_parser.hpp"
+#include "../utils/file_system.hpp"
 #include "CLI/CLI.hpp"
 #include <cstdlib>
 #include <string>
 #include <tuple>
-#include "../utils/file_system.hpp"
 
 namespace aix {
-  ConfigOptions ArgParser::ParseArgument(const int argc, char** argv, ConfigOptions& options) {
+ConfigOptions ArgParser::ParseArgument(const int argc, char **argv, ConfigOptions &options) {
 
     if (aix.get_option_no_throw("-p") == nullptr) {
-      aix.add_option("-p", options.prompt, "Prompt message");
+        aix.add_option("-p", options.prompt, "Prompt message");
     }
 
     if (aix.get_option_no_throw("--sed") == nullptr) {
-      aix.add_option("--sed", options.sed_target, "Select lines inside files by numbers");
+        aix.add_option("--sed", options.sed_target, "Select lines inside files by numbers");
     }
-    
+
     if (aix.get_option_no_throw("--file-path") == nullptr) {
-      aix.add_option("--file-path", options.file_path, "Location of the file");
+        aix.add_option("--file-path", options.file_path, "Location of the file");
     }
 
     try {
-      aix.parse(argc, argv);
-      std::tie(options.sed_start_line, options.sed_end_line) = aix::ExtractSedNumbers(options.sed_target);
+        aix.parse(argc, argv);
+        std::tie(options.sed_start_line, options.sed_end_line) =
+            aix::ExtractSedNumbers(options.sed_target);
     } catch (const CLI::ParseError &e) {
-      aix.exit(e);
+        aix.exit(e);
     }
 
     return options;
-  }
 }
+} // namespace aix
